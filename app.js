d1efe6876b391eb64f0c8af4305074c663aea4f5
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-19 17:20:22 +0200

    build: express setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+//json
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
