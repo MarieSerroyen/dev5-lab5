e046e1f96cbe23240856c7c7fa0b323a53cd28ec
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-23 14:57:11 +0200

    feat: require cors

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const express = require('express')
+const express = require('express');
 const messages = require('./routes/messages');
+const cors = require('cors');
 
-
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 //json
-app.use(express.json())
-app.set('view engine', 'jade')
+app.use(express.json());
+app.use(cors());
+app.set('view engine', 'jade');
 
 
 app.use('/api/v1/messages', messages);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Hello', message: 'user' });
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
 
