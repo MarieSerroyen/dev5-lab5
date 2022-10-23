661b1ea48b0033b859e2aff172074d1abda48c1a
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-23 15:46:10 +0200

    feat: change scripts in package.json

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const messages = require('./routes/messages');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
 
 //json
 app.use(express.json());
@@ -13,15 +12,9 @@ app.set('view engine', 'jade');
 
 app.use('/api/v1/messages', messages);
 
-app.use(function(req, res, next){
-  next(createError(404));
-});
-
 app.get('/', (req, res) => {
   res.render('index', { title: 'Hello', message: 'user' });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+module.exports = app;
 
