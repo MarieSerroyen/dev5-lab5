3c6b7297fa7447df4d3bd6387eb01f49d649a8c1
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-23 15:56:42 +0200

    feat: added error handeler

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const express = require('express');
 const messages = require('./routes/messages');
 const cors = require('cors');
@@ -15,6 +16,23 @@ app.use('/api/v1/messages', messages);
 app.get('/', (req, res) => {
   res.render('index', { title: 'Hello', message: 'user' });
 });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 module.exports = app;
 
