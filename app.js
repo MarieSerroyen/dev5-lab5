f2dea59861c62f30871edaabb3744315a52e619a
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-23 12:57:06 +0200

    feat: show simple homepage

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const port = 3000
 
 //json
 app.use(express.json())
+app.set('view engine', 'jade')
 
 
 app.use('/api/v1/messages', messages);
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Hello', message: 'user' });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
